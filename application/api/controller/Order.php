c10cebbcb144ca22522e407916f4d4c471e59956
Duncan <1466863073@qq.com>
2019-03-22 19:11:24 +0800

    Update:支付成功

---
+++ application/api/controller/Order.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Duncan
+ * Date: 2019/3/22
+ * Time: 18:59
+ */
+namespace api\controller;
+use think\Controller;
+class Order extends Controller{
+    private $obj = null;
+    public function initialize() {
+        $this->obj = model('Order');
+    }
+
+    public function payStatus() {
+        $id = input('post.id', 0, 'intval');
+        if (!$id) {
+            return showStatus(0, 'error');
+        }
+
+        //判定是否登陆
+        $order = $this->obj->get($id);
+        if ($order->pay_status == 1) {
+            return showStatus(1, 'success');
+        }
+
+        return showStatus(0, 'error');
+    }
+}
